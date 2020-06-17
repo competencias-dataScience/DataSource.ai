541df4a86ef44bf31e7b48fd860d76bd1e0b559f
Edimer <edimerfrv@gmail.com>
2020-06-17 14:41:07 -0500

    catBoost 5 best tuning

---
+++ IncludeFinanceLatin/codeR/catboost5.R
@@ -0,0 +1,65 @@
+# Cargando datos
+library(data.table)
+library(tidyverse)
+load("myDataTrain.Rdata")
+load("myDataTest.Rdata")
+dataSample <- fread("../data/sample.csv")
+
+
+# Bibliotecas
+library(caret)
+library(catboost)
+
+# Datos train
+dataTrain <- as.data.frame(myDataTrain) %>% 
+  select(-c(newID, uniqueid))
+
+# Partición de datos (80% train - 20% test)
+set.seed(123)
+indx <- createDataPartition(y = dataTrain$target, times = 1, p = 0.8, list = FALSE)
+dfTrain <- dataTrain[indx, ]
+dfTest <- dataTrain[-indx, ]
+
+# Datos para catboost
+train_pool <- catboost.load_pool(data = dfTrain[, -1], label = dfTrain[, 1])
+test_pool <- catboost.load_pool(data = dfTest[, -1], label = dfTest[, 1])
+
+# Ajuste de modelo
+fit_params <- list(iterations = 5000,
+                   loss_function = 'Logloss',
+                   depth = 6,
+                   border_count = 32,
+                   rsm = 0.5,
+                   l2_leaf_reg = 3,
+                   learning_rate = 0.01,
+                   od_type = 'Iter',
+                   use_best_model = TRUE,
+                   od_wait = 500)
+modelo <- catboost.train(train_pool, test_pool, fit_params)
+
+# Predicciones
+predicciones1 <- catboost.predict(modelo, test_pool,
+                                  prediction_type = 'Probability')
+
+predicciones2 <- catboost.predict(modelo, test_pool,
+                                  prediction_type = 'Class')
+
+# Confusion matrix
+confusionMatrix(data = factor(predicciones2),
+                reference = factor(dfTest$target),
+                positive = "1")
+
+# Accuracy: 0.8869
+# Kappa: 0.4209
+
+# Predicciones submission
+myDataTest2 <- myDataTest %>% select(-c(newID, uniqueid))
+test_pool2 <- catboost.load_pool(myDataTest2)  
+predicciones3 <- catboost.predict(modelo, test_pool2,
+                                  prediction_type = 'Class')
+
+# Submission
+dataCatboost5 <- myDataTest %>% 
+  mutate(bank_account = predicciones3) %>% 
+  select(uniqueid = newID, bank_account)
+write.csv(dataCatboost5, file = "submission/catB5.csv", row.names = FALSE)

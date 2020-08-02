fc091a98396b34eb0dd22bcd9bd2d526b812386f
Edimer <edimerfrv@gmail.com>
2020-08-02 11:34:39 -0500

    exploratory advances

--- PreciosApartamentos/codeR/pruebas.R
+++
@@ -1,4 +0,0 @@
-library(data.table)
-train <- fread("../data/train.csv", encoding = "UTF-8")
-test <- fread("../data/test.csv", encoding = "UTF-8")
-sampleSub <- fread("../data/sampleSub.csv", encoding = "UTF-8")

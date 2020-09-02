7c2589bc4ebb47cca093f9d9b5bbeffe2e7bdcef
Edimer <edimerfrv@gmail.com>
2020-09-02 12:02:04 -0500

    more models

---
+++ PreciosApartamentos/codeR/LightGBM10.R
@@ -0,0 +1,102 @@
+#  ----- Bibliotecas ------
+library(lightgbm)
+library(data.table)
+library(tidyverse)
+library(fastDummies)
+
+# ------- Test ----
+load("newTest1.Rdata")
+test <- newTest1 %>% 
+  mutate(rooms = factor(rooms),
+         bedrooms = factor(bedrooms),
+         bathrooms = factor(bathrooms)) %>% 
+  mutate_if(is.character, as.factor) %>% 
+  as.data.frame() 
+
+test_dummy <- dummy_cols(test) %>% 
+  select(-c(rooms, bedrooms, bathrooms, pais, provincia_departamento,
+            ciudad, Capital, surfaceClass))
+
+# ------- Train ----
+load("newTrain1.Rdata")
+train <- newTrain1 %>% 
+  mutate(rooms = factor(rooms),
+         bedrooms = factor(bedrooms),
+         bathrooms = factor(bathrooms)) %>% 
+  mutate_if(is.character, as.factor) %>% 
+  relocate(price) %>% 
+  as.data.frame()
+
+train_dummy <- dummy_cols(train) %>% 
+  select(-c(rooms, bedrooms, bathrooms, pais, provincia_departamento,
+            ciudad, Capital, surfaceClass))
+
+train_dummy2 <- train_dummy[, names(train_dummy) %in% names(test_dummy)]
+train_dummy2$price <- train$price
+
+test_dummy2 <-  test_dummy[, names(test_dummy) %in% names(train_dummy2)]
+
+# ------- Sample  ----
+sampleSub <- fread("../data/sampleSub.csv", encoding = "UTF-8")
+
+# caret for partition data
+library(caret)
+set.seed(123)
+indx <- createDataPartition(y = train_dummy2$price, times = 1, p = 0.80,
+                            list = FALSE)
+dfTrain <- train_dummy2[indx, ]
+dfTest <- train_dummy2[-indx, ]
+
+# Data for lightgbm
+dataTrain_lgbm <- lgb.Dataset(data = data.matrix(dfTrain[, -238]),
+                              label = dfTrain[, 238])
+dataTest_lgbm <- lgb.Dataset(data = data.matrix(dfTest[, -238]),
+                             label = dfTest[, 238])
+
+# Parameters for lightgbm
+myParams <- list(
+  boosting = "gbdt",
+  objective = "poisson",
+  metric = "rmse",
+  learning_rate = 0.01,
+  feature_fraction = 1,
+  bagging_fraction = 1
+)
+
+# Train model
+modelo <- lgb.train(params = myParams,
+                    data = dataTrain_lgbm,
+                    nrounds = 10000,
+                    valids = list(test = dataTest_lgbm),
+                    early_stopping_rounds = 500)
+
+# Best iter:5529
+# Best score: 67501.01
+
+# Predictions
+predicciones <- predict(modelo, data.matrix(test_dummy2), 
+                        num_iteration = modelo$best_iter)
+
+predicciones[predicciones < 0] <- 0
+x11();hist(predicciones)
+
+# Submission
+sampleSub %>% 
+  select(Id) %>% 
+  mutate(price = predicciones) %>%
+  rename(id = Id) %>% 
+  as.data.frame() ->
+  lgbmR10
+
+# Export submission for zindi
+write.csv(lgbmR10, file = "submission/lgbmR10Aptos.csv", row.names = FALSE)
+
+# Importance variables
+impModelo <- lgb.importance(modelo, percentage = TRUE)
+x11()
+impModelo %>% 
+  slice(1:50) %>% 
+  ggplot(data = ., aes(x = reorder(Feature,Gain), y = Gain)) +
+  coord_flip() + 
+  geom_col(color = "black") +
+  theme_light()
---
+++ PreciosApartamentos/codeR/LightGBM12.R
@@ -0,0 +1,105 @@
+#  ----- Bibliotecas ------
+library(lightgbm)
+library(data.table)
+library(tidyverse)
+library(fastDummies)
+
+# ------- Test ----
+load("newTest1.Rdata")
+test <- newTest1 %>% 
+  mutate(rooms = factor(rooms),
+         bedrooms = factor(bedrooms),
+         bathrooms = factor(bathrooms)) %>% 
+  mutate_if(is.character, as.factor) %>% 
+  as.data.frame() 
+
+test_dummy <- dummy_cols(test) %>% 
+  select(-c(rooms, bedrooms, bathrooms, pais, provincia_departamento,
+            ciudad, Capital, surfaceClass))
+
+# ------- Train ----
+load("newTrain1.Rdata")
+train <- newTrain1 %>% 
+  mutate(rooms = factor(rooms),
+         bedrooms = factor(bedrooms),
+         bathrooms = factor(bathrooms)) %>% 
+  mutate_if(is.character, as.factor) %>% 
+  relocate(price) %>% 
+  as.data.frame()
+
+train_dummy <- dummy_cols(train) %>% 
+  select(-c(rooms, bedrooms, bathrooms, pais, provincia_departamento,
+            ciudad, Capital, surfaceClass))
+
+train_dummy2 <- train_dummy[, names(train_dummy) %in% names(test_dummy)]
+train_dummy2$price <- train$price
+
+test_dummy2 <-  test_dummy[, names(test_dummy) %in% names(train_dummy2)]
+
+# ------- Sample  ----
+sampleSub <- fread("../data/sampleSub.csv", encoding = "UTF-8")
+
+# caret for partition data
+library(caret)
+set.seed(123)
+indx <- createDataPartition(y = train_dummy2$price, times = 1, p = 0.80,
+                            list = FALSE)
+dfTrain <- train_dummy2[indx, ]
+dfTest <- train_dummy2[-indx, ]
+
+# Data for lightgbm
+dataTrain_lgbm <- lgb.Dataset(data = data.matrix(dfTrain[, -238]),
+                              label = dfTrain[, 238])
+dataTest_lgbm <- lgb.Dataset(data = data.matrix(dfTest[, -238]),
+                             label = dfTest[, 238])
+
+# Parameters for lightgbm
+myParams <- list(
+  boosting = "gbdt",
+  objective = "poisson",
+  metric = "rmse",
+  learning_rate = 0.01,
+  feature_fraction = 0.5,
+  bagging_fraction = 1,
+  min_data_in_leaf = 100,
+  num_leaves = 255,
+  max_depth = -1
+)
+
+# Train model
+modelo <- lgb.train(params = myParams,
+                    data = dataTrain_lgbm,
+                    nrounds = 20000,
+                    valids = list(test = dataTest_lgbm),
+                    early_stopping_rounds = 500)
+
+# Best iter:5127
+# Best score: 69391.22
+
+# Predictions
+predicciones <- predict(modelo, data.matrix(test_dummy2), 
+                        num_iteration = modelo$best_iter)
+
+predicciones[predicciones < 0] <- 0
+x11();hist(predicciones)
+
+# Submission
+sampleSub %>% 
+  select(Id) %>% 
+  mutate(price = predicciones) %>%
+  rename(id = Id) %>% 
+  as.data.frame() ->
+  lgbmR12
+
+# Export submission for zindi
+write.csv(lgbmR12, file = "submission/lgbmR12Aptos.csv", row.names = FALSE)
+
+# Importance variables
+impModelo <- lgb.importance(modelo, percentage = TRUE)
+x11()
+impModelo %>% 
+  slice(1:50) %>% 
+  ggplot(data = ., aes(x = reorder(Feature,Gain), y = Gain)) +
+  coord_flip() + 
+  geom_col(color = "black") +
+  theme_light()
